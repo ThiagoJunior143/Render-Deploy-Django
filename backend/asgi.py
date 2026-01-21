182b3d8ab60d5d83967f8620b6d4c9912d095047
ThiagoJunior143 <youremail@example.com>
2026-01-21 18:37:46 +0100

    modified asgi.py wsgi.py and manage.py files

--- backend/asgi.py
+++ backend/asgi.py
@@ -10,7 +10,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-settings_module = 'api.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'backend.settings'
+settings_module = 'backend.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'backend.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',settings_module )
 
 
